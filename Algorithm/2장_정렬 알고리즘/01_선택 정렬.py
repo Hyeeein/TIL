71fd0caf0a464f18fe8977338a32370f3bfe6ed2
Hyeeein <phi498@naver.com>
2022-09-14 15:30:55 +0900

    selectionSort

--- Algorithm/2장_정렬 알고리즘/01_선택 정렬.py
+++ Algorithm/2장_정렬 알고리즘/01_선택 정렬.py
@@ -10,4 +10,42 @@
 #     }
 # end selectionSort()
 
-### 선택 정렬 프로그램
+# -----------------------------------------------------------
+
+### 정렬 선택 프로그램
+
+import random, time
+
+# 선택 정렬 함수
+def selectionSort(a, n):
+    for i in range(1, n):
+        minIndex = i
+        for j in range(i+1, n+1):
+            if a[minIndex] > a[j]: minIndex = j
+            a[minIndex], a[i] = a[i], a[minIndex]
+
+# 정렬 함수 오류 검사 함수
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if (a[i] > a[i+1]):
+            isSorted = False
+        if (not isSorted):
+            break
+    if isSorted: print('정렬 완료')
+    else: print('정렬 오류 발생')
+
+# 선택 정렬 실행 시간 출력 (원소의 개수 5000개)
+N = 5000
+a = []
+a.append(None)
+
+for i in range(N):
+    a.append(random.randint(1, N))
+
+start_time = time.time()
+selectionSort(a, N)
+end_time = time.time() - start_time
+
+print('선택 정렬의 실행 시간 (N = %d) : %0.3f' %(N, end_time))
+checkSort(a, N)
