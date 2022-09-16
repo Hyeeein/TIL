75956cb19784487ca36bc1f57937a5afe499a95a
Hyeeein <phi498@naver.com>
2022-09-16 15:50:03 +0900

    shellSort

--- Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
+++ Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
@@ -2,29 +2,33 @@
 
 ### 알고리즘 (ADL)
 # shellSort(a[], n)
-#     for (h <- n/2; h > 0; h = h/2) do {     // h 값 계산, h 값을 감소시키며 진행
-#
-#         // 왼쪽에서 출발하는 i
-#         for (i <- 0; i < h; i <- i + 1) do {
-# 
-#             // 오른쪽에서 출발하는 j (h 간격에 있는 데이터)
-#             for (j = i + h; j < n; j = j + h) do {
-#                 tmp = a[j];
-#                 tmp_index = j;
-# 
-#                 // a[i]가 a[j]보다 클 경우 찾기
-#                 while (tmp_index > h-1 and a[i] > tmp) do {
-#                     a[j] = a[i];
-#                     j = i;
-#                 }
-#                 a[i] = tmp
+#     for (h <- 1; h < n; h <- 3*h + 1) do {};  // 첫 번째 h 값 계산
+#     for ( ; h > 0; h <- h/3) do {             // h 값을 감소시키며 진행
+#         for (i <- h + 1; i <= n; i <- i + 1) do {
+#             v <- a[i];
+#             j <- i;
+#             while (j <- h and a[j-h] > v) do {
+#                 a[j] <- a[j-h];
+#                 j <- j-h;
 #             }
+#             A[j] <- v;
 #         }
 #     }
 # end shellSort()
 
 
 ### 파이썬 프로그래밍
-def shellSort(a, n):
 
-    
+# 쉘 정렬 함수
+def shellSort(a, n):
+    h = 1
+    while h < n:
+        h = 3 * h + 1
+    while h > 0:
+        for i in range(h+1, n+1):
+            v, j = a[i], i
+            while j > h and a[j-h] > v:
+                a[j] = a[j-h]
+                j -= h
+            a[j] = v
+        h = int(h/3)
