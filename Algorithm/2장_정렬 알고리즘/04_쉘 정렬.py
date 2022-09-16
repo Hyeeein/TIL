8ad71450638a296d860f48b85ac164d7ecb651b2
Hyeeein <phi498@naver.com>
2022-09-16 15:53:59 +0900

    shellSort

--- Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
+++ Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
@@ -32,3 +32,28 @@ def shellSort(a, n):
                 j -= h
             a[j] = v
         h = int(h/3)
+
+# 정렬 오류 확인 함수
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if (a[i] > a[i+1]):
+            isSorted = False
+        if (not isSorted):
+            break
+    
+    if isSorted: print('정렬 완료')
+    else: print('정렬 오류 발생')
+
+import random, time
+
+N = 5000
+a = []
+a.append(-1)
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+shellSort(a, N)
+end_time = time() - start_time
+print('쉘 정렬의 실행 시간 (N = %d) : %0.3f' % (N, end_time))
+checkSort(a, N)
