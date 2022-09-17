cd2ec73f3f603f4ea544ea527a8c1362010ac075
Hyeeein <phi498@naver.com>
2022-09-17 21:04:40 +0900

    update 0917

--- Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
+++ Algorithm/2장_정렬 알고리즘/04_쉘 정렬.py
@@ -54,6 +54,6 @@ for i in range(N):
     a.append(random.randint(1, N))
 start_time = time.time()
 shellSort(a, N)
-end_time = time() - start_time
+end_time = time.time() - start_time
 print('쉘 정렬의 실행 시간 (N = %d) : %0.3f' % (N, end_time))
 checkSort(a, N)
